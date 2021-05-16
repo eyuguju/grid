e9c2c9a03a2e2e14e5a3fa47c509ac4b19558d5d
鳄鱼故居 <1565167748@qq.com>
2021-05-16 17:24:34 +0800

    Revert "v0"
    This reverts commit c55d50698afbd026880b1df4c86f906898016e78.

--- stage3/ImageReader/ImageReaderRunner.java
+++
@@ -1,9 +0,0 @@
-import imagereader.Runner;
-public class ImageReaderRunner 
-{
-      public static void main(String[] args) {
-           ImplementImageIO imageioer = new ImplementImageIO();
-           ImplementImageProcessor processor = new ImplementImageProcessor();
-           Runner.run(imageioer, processor);
-      }
-}
